982a2cc5b49e6694fcc63d1dbceff5560498f343
Luca Giaccone <luca.giaccone@polito.it>
2023-09-11 21:27:08 +0200

    Modify 'permission.py' (because 'can_send_media_messages' is deprecated since PTB v20.5)

--- util/permission.py
+++ util/permission.py
@@ -2,10 +2,9 @@ from telegram import ChatPermissions
 
 initial_permission = ChatPermissions(can_send_messages=False)
 standard_permissions = ChatPermissions(can_send_messages=True,
-                can_send_media_messages=True,
-                can_send_polls=True,
-                can_send_other_messages=True,
-                can_add_web_page_previews=True,
-                can_change_info=False,
-                can_invite_users=False,
-                can_pin_messages=False)
+                                       can_send_other_messages=True,
+                                       can_send_polls=True,
+                                       can_add_web_page_previews=True,
+                                       can_change_info=False,
+                                       can_invite_users=False,
+                                       can_pin_messages=False)
